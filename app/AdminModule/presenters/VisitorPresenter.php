d4dec11f88050a44baa3a88cf36794a11bac5f91
ales <onlyluckcze@gmail.com>
2017-12-08 16:26:38 +0100

    visitors

---
+++ app/AdminModule/presenters/VisitorPresenter.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AdminModule\Presenters;
+
+/**
+ * VisitorPresenter Presenter class
+ */
+class VisitorPresenter extends SecurePresenter
+{
+	/**
+	 * @var
+	 * \AdminModule\Forms\VisitorForm
+	 */
+	public $visitorForm;
+
+	/**
+	 * @var
+	 * \AdminModule\Grids\VisitorGrid
+	 */
+	public $visitorGrid;
+
+	/**
+	 * @var
+	 * \Repository\Visitor
+	 */
+	public $visitorRepository;
+
+
+	public function __construct(\AdminModule\Grids\VisitorGrid $visitorGrid, \AdminModule\Forms\VisitorForm $visitorForm, \Repository\Visitor $visitorRepository)
+	{
+		$this->visitorGrid = $visitorGrid;
+		$this->visitorRepository = $visitorRepository;
+		$this->visitorForm = $visitorForm;
+	}
+
+
+	public function beforeRender()
+	{
+		$this->template->title = "Visitor";
+	}
+
+
+	public function renderDetail($id)
+	{
+		$data = $this->visitorRepository->getSingle($id);
+		        $this->template->data = $data;
+	}
+
+
+	public function renderEdit($id)
+	{
+		$data = $this->visitorRepository->getFormDataById($id);
+
+		$this->getComponent("edit")->setDefaults($data);
+	}
+
+
+	public function handleDelete($id)
+	{
+		$this->visitorRepository->removeById($id);
+	}
+
+
+	public function createComponentNew()
+	{
+		$form = $this->visitorForm->create();
+
+		$form->onSuccess[] = function($form, $values) {
+		    $entity = NULL;
+
+		    try {
+		        $entity = $this->visitorRepository->insertForm($form->getValues(TRUE));
+		        $this->flashMessage("Uloženo", "success");
+		    } catch (\Exception $ex) {
+		        $this->flashMessage("Nastala chyba. ({$ex->getMessage()})");
+		    }
+
+		    if ($entity !== NULL) {
+		            if ($form["submit_stay"]->isSubmittedBy()) {
+		                $this->redirect("edit", array("id" => $entity->id));
+		            } else if ($form["submit"]->isSubmittedBy()) {
+		                $this->redirect("default");
+		            }
+		      }
+		};
+
+		return $form;
+	}
+
+
+	public function createComponentEdit()
+	{
+		$form = $this->visitorForm->create();
+
+		$form->onSuccess[] = function($form, $values) {
+		    $entity = NULL;
+
+		    try {
+		        $entity = $this->visitorRepository->updateForm($form->getValues(TRUE), $values["id"]);
+		        $this->flashMessage("Uloženo", "success");
+		    } catch (\Exception $ex) {
+		        $this->flashMessage("Nastala chyba. ({$ex->getMessage()})");
+		    }
+
+		    if ($entity !== NULL) {
+		        if ($form["submit_stay"]->isSubmittedBy()) {
+		            $this->redirect("edit", array("id" => $entity->id));
+		        } else if ($form["submit"]->isSubmittedBy()) {
+		            $this->redirect("default");
+		        }
+		    }
+		};
+
+		return $form;
+	}
+
+
+	public function createComponentGrid()
+	{
+		$source = $this->visitorRepository->getAll(TRUE);
+
+		return $this->visitorGrid->create($source);
+	}
+}
---
+++ app/entity/Visitor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\OneToOne;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Visitor
+ *
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Entity()
+ */
+class Visitor extends BaseEntity
+{
+	/** @ORM\Column(type="string") */
+	protected $title;
+
+
+}
